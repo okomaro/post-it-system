22ccabbec9c2e8262b71a940c8f2366f6edaf9f6
okomaro <okomaro@gmail.com>
2018-09-10 21:54:05 +0200

    post-it can be deleted by logedin user

--- delete_postit.php
+++ delete_postit.php
@@ -1,8 +1,8 @@
+<?php session_start(); ?>
 <?php	
 	if(isset($_SESSION['uid'])){ 
-
 		$postitid = filter_input(INPUT_POST,'pid', FILTER_VALIDATE_INT) or die('MiSSing something');
-		$userid = $_SESSION['uid'] or die('you should log in first');
+		$userid = $_SESSION['uid'];
 	
 		require_once('dbcon.php');
 
@@ -10,15 +10,11 @@
 	  	$stmt = $link->prepare($sql);
     	$stmt->bind_param('ii', $postitid, $userid);
 		$stmt->execute();
-//		echo "deleted!";
-		header('HTTP/1.1 303 See other');
 		header('Location: postit_board.php');
 		exit();
-
-
-		}
-		
-	else {
-		echo 'You can not delete other peoples post-its';
 	}
-//				
+else {
+	echo 'it is not your post';
+}
+
+
